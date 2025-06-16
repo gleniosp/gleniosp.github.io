6b641592dd9dbcb5b814a5eafd50d864a4d07055
gleniosp <11273108+gleniosp@users.noreply.github.com>
2025-06-16 15:01:28 +0200

    Add tailwind and global style

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,57 +2,64 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightBlog from 'starlight-blog'
+import tailwindcss from '@tailwindcss/vite';
 
 import { BLOG_URL, GITHUB_URL, LOCAL_PORT } from './src/constants';
 
 const getSite = () => {
-	return import.meta.env.DEV ? `http://localhost:${LOCAL_PORT}/` : BLOG_URL
+    return import.meta.env.DEV ? `http://localhost:${LOCAL_PORT}/` : BLOG_URL
 }
 
 // https://astro.build/config
 export default defineConfig({
-	server: { port: LOCAL_PORT },
-	site: getSite(),
-	integrations: [
-		starlight({
-			credits: true,
-			title: 'gleniosp',
-			locales: {
-				root: { label: 'English', lang: 'en' },
-				// temporarily disable extra languages as it'll require more work for the RSS part and the rest of theme
-				// 'pt-br': { label: 'Português do Brasil', lang: 'pt-BR' }, 
-			},
-			social: [
-				{
-					icon: 'github',
-					label: 'GitHub',
-					href: 'https://github.com/gleniosp'
-				},
-				{
-					icon: 'linkedin',
-					label: 'LinkedIn',
-					href: GITHUB_URL
-				}
-			],
-			sidebar: [],
-			components: {
-				Footer: './src/components/ConditionalFooter.astro',
-			},
-			plugins: [
-				starlightBlog({
-					authors: {
-						gleniosp: {
-							name: 'Glênio Silva Pimentel',
-							title: 'Software Engineer',
-							picture: `${GITHUB_URL}.png`,
-							url: getSite(),
-						},
-					},
-					metrics: {
-						readingTime: true,
-					},
-				})
-			],
-		}),
-	],
+    server: { port: LOCAL_PORT },
+    site: getSite(),
+    integrations: [
+        starlight({
+            credits: true,
+            title: 'gleniosp',
+            customCss: [
+                './src/styles/global.css',
+            ],
+            locales: {
+                root: { label: 'English', lang: 'en' },
+                // temporarily disable extra languages as it'll require more work for the RSS part and the rest of theme
+                // 'pt-br': { label: 'Português do Brasil', lang: 'pt-BR' }, 
+            },
+            social: [
+                {
+                    icon: 'github',
+                    label: 'GitHub',
+                    href: 'https://github.com/gleniosp'
+                },
+                {
+                    icon: 'linkedin',
+                    label: 'LinkedIn',
+                    href: GITHUB_URL
+                }
+            ],
+            sidebar: [],
+            components: {
+                Footer: './src/components/ConditionalFooter.astro',
+            },
+            plugins: [
+                starlightBlog({
+                    authors: {
+                        gleniosp: {
+                            name: 'Glênio Silva Pimentel',
+                            title: 'Software Engineer',
+                            picture: `${GITHUB_URL}.png`,
+                            url: getSite(),
+                        },
+                    },
+                    metrics: {
+                        readingTime: true,
+                    },
+                })
+            ],
+        }),
+    ],
+    vite: {
+        plugins: [tailwindcss()]
+    }
 });
