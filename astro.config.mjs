1cb8cd5f9ebb36b028304a66eaabef5a35749b0c
gleniosp <11273108+gleniosp@users.noreply.github.com>
2025-06-18 20:37:37 +0200

    Add  integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightBlog from 'starlight-blog'
 import tailwindcss from '@tailwindcss/vite';
+import alpinejs from '@astrojs/alpinejs';
 
 import { BLOG_URL, GITHUB_URL, LOCAL_PORT } from './src/constants';
 
@@ -14,51 +15,49 @@ const getSite = () => {
 export default defineConfig({
     server: { port: LOCAL_PORT },
     site: getSite(),
-    integrations: [
-        starlight({
-            credits: true,
-            title: 'gleniosp',
-            customCss: [
-                './src/styles/global.css',
-            ],
-            locales: {
-                root: { label: 'English', lang: 'en' },
-                // temporarily disable extra languages as it'll require more work for the RSS part and the rest of theme
-                // 'pt-br': { label: 'Português do Brasil', lang: 'pt-BR' }, 
+    integrations: [starlight({
+        credits: true,
+        title: 'gleniosp',
+        customCss: [
+            './src/styles/global.css',
+        ],
+        locales: {
+            root: { label: 'English', lang: 'en' },
+            // temporarily disable extra languages as it'll require more work for the RSS part and the rest of theme
+            // 'pt-br': { label: 'Português do Brasil', lang: 'pt-BR' }, 
+        },
+        social: [
+            {
+                icon: 'github',
+                label: 'GitHub',
+                href: 'https://github.com/gleniosp'
             },
-            social: [
-                {
-                    icon: 'github',
-                    label: 'GitHub',
-                    href: 'https://github.com/gleniosp'
+            {
+                icon: 'linkedin',
+                label: 'LinkedIn',
+                href: GITHUB_URL
+            }
+        ],
+        sidebar: [],
+        components: {
+            Footer: '@components/ConditionalFooter.astro',
+        },
+        plugins: [
+            starlightBlog({
+                authors: {
+                    gleniosp: {
+                        name: 'Glênio Silva Pimentel',
+                        title: 'Software Engineer',
+                        picture: `${GITHUB_URL}.png`,
+                        url: getSite(),
+                    },
                 },
-                {
-                    icon: 'linkedin',
-                    label: 'LinkedIn',
-                    href: GITHUB_URL
-                }
-            ],
-            sidebar: [],
-            components: {
-                Footer: './src/components/ConditionalFooter.astro',
-            },
-            plugins: [
-                starlightBlog({
-                    authors: {
-                        gleniosp: {
-                            name: 'Glênio Silva Pimentel',
-                            title: 'Software Engineer',
-                            picture: `${GITHUB_URL}.png`,
-                            url: getSite(),
-                        },
-                    },
-                    metrics: {
-                        readingTime: true,
-                    },
-                })
-            ],
-        }),
-    ],
+                metrics: {
+                    readingTime: true,
+                },
+            })
+        ],
+    }), alpinejs()],
     vite: {
         plugins: [tailwindcss()]
     }
